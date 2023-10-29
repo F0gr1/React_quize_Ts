fc5902fa19b2a63a838a99d3f4b34fd74f2141ec
F0gr1 <ishigami0314@icloud.com>
2023-10-29 13:37:53 +0900

    update Quize の正解と不正解

--- src/QuizForm.tsx
+++ src/QuizForm.tsx
@@ -10,13 +10,13 @@ const QuizForm : React.FC<QuizProps> = ({ username }) =>{
   const [error] = useState<Error | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
-
   const fetchData = async () => {
     try {
+      // クイズ問題をfetch してくる
       const response = await fetch('https://opentdb.com/api.php?amount=10&category=15&type=multiple');
-      
       const data = await response.json();
-      console.log(data.results);
+
+      //クイズデータセット
       setQuizData(data.results);
     } catch (error) {
       
@@ -24,23 +24,44 @@ const QuizForm : React.FC<QuizProps> = ({ username }) =>{
       setLoading(false);
     }
   };
-
+  
   useEffect(() => {
     fetchData();
   }, []);
 
   if (loading) {
-    return <p>Loading...</p>;
+    return <h1>Loading...</h1>;
   }
 
   if (error) {
-    return <p>Error: {error.message}</p>;
+    return <h1>Error: {error.message}</h1>;
   }
 
   if (!quizData) {
-    return <p>No quiz data available.</p>;
+    return <h1>No quiz data available.</h1>;
   }
+  const allAnswers = [
+    ...quizData[currentQuestionIndex].incorrect_answers,
+    quizData[currentQuestionIndex].correct_answer
+  ];
+  
+  // 選択肢をシャッフルする関数
+  const shuffle = (array: any[]) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+  
+  const shuffledAnswers = shuffle([...allAnswers]); // 新しい配列としてシャッフル
+  
   const handleAnswer = (ans: string) => {
+    if(ans === quizData[currentQuestionIndex].correct_answer){
+        alert("正解");
+    }else{
+      alert("不正解");
+    }
     if (currentQuestionIndex < quizData.length - 1) {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
       alert("次のクイズ");
@@ -51,18 +72,17 @@ const QuizForm : React.FC<QuizProps> = ({ username }) =>{
   return (
     <div>
         <div>
-          <p className="p-3">{ quizData[currentQuestionIndex].question } </p>
+        <p className="p-3">{quizData[currentQuestionIndex].question}</p>
           <div className="grid grid-cols-2 bg-emerald-50 p-1">
-            <div className="border-solid border-2 p-4" onClick={() => handleAnswer(quizData[currentQuestionIndex].correct_answer)} >
-              { quizData[currentQuestionIndex].correct_answer}
-            </div>
-            {
-              quizData[currentQuestionIndex].incorrect_answers.map((ans: string, ansIndex: number) => (
-              <div key={ansIndex}  onClick={() => handleAnswer(ans)} className="border-solid border-2 p-4">
+            {shuffledAnswers.map((ans, ansIndex) => (
+              <div
+                key={ansIndex}
+                onClick={() => handleAnswer(ans)}
+                className="border-solid border-2 p-4"
+              >
                 {ans}
               </div>
-              ))
-            }
+            ))}
           </div>
         </div>
     </div>
